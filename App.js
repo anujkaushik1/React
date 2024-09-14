5c3683bd7f06fb8d8400bfe60aeb309f91e7f57c
anujkaushik1512 <anujkaushik1512@gmail.com>
2024-09-14 23:31:46 +0530

    mutliple children

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-
-
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "injecting_heading" }, "hello anuj")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "injecting_heading" }, "hello anuj"),
+    React.createElement("h1", { id: "injecting_heading" }, "diffrent h2"),
+  ])
 );
 
 console.log(parent);
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
