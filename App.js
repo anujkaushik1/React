9cae87792b66472235e868e2a5b97c77dc80e0dc
anujkaushik1512 <anujkaushik1512@gmail.com>
2024-09-15 22:45:42 +0530

    react element inside component

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
+const jsxHeading = <h1 class="world ">Hello world Anuj</h1>;
 
-const jsxHeading = <h1>Hello world Anuj</h1>
-
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      {jsxHeading}
+      <h1>This is heading comp</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
