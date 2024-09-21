db07cbf57bc1e435f65f67fffffe45fdea195bda
anujkaushik1512 <anujkaushik1512@gmail.com>
2024-09-21 13:52:59 +0530

    outlet

--- App.js
+++ App.js
@@ -2,31 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Error from "./Components/Error";
-
 
 const AppLayout = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet/>
     </div>
   );
 };
 
 export default AppLayout;
 
-
 const appRouter = createBrowserRouter([
-  {path: '/', element: <AppLayout/>, errorElement: <Error/>},
-  {path: '/about', element: <About/>},
-  {path: '/contact', element: <Contact/>}
-])
-
-
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Body /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+  {
+    path: "/testing",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/testing", element: <div>Testing world</div> },
+      { path: "/testing/about", element:<About/> },
+ 
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);
